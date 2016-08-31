8bccaa39af8d672e0f0d9081ed765a4821a9a934
Kevin Yamada <kyamada@roadtripnation.com>
2016-08-30 23:17:03 -0700

    Testing webpack + react

---
+++ index.jsx
@@ -0,0 +1,5 @@
+/** @jsx React.DOM */
+'use strict'
+var React = require('react')
+var Hello = require('./src/Components/GifShow')
+React.renderComponent(<GifShow />, document.getElementById('content'))
---
+++ src/Components/GifShow.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+
+
+class GifShow extends React.Component {
+    render() {
+        return <div><img src="http://i.giphy.com/eSfChHkLtJI5i.gif" /></div>;
+    }
+}
